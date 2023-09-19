9d7f44ff8dcfd741828d544635b92d22449b9cf9
Hammad Nadir <Hammad.Nadir@Biotech.com>
2023-09-19 23:53:50 +0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,12 @@ export default function Home() {
         <title>Bites Cake Crafters</title>
         <meta name="description" content="Bites Cake Crafters is providing homemade baked goodies for every taste." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://www.logomaker.com/api/main/images/1j+ojFVDOMkX9Wytexe43D6kh...OGrxFHnR...OwXs1M3EMoAJtlikoh...Zi9...o7" />
       </Head>
       <main>
         <section class="landing">
           <div class="landing-inner">
-            <img class="logo" src="/logo.png" alt="TheLittleBakery" />
+            <img class="logo" src="https://www.logomaker.com/api/main/images/1j+ojFVDOMkX9Wytexe43D6kh...OGrxFHnR...OwXs1M3EMoAJtlikoh...Zi9...o7" alt="TheLittleBakery" />
             <p>Homemade Baked Goodies For Every Taste</p>
             <h1>Coming Soon</h1>
             <div class="countdown"></div>
